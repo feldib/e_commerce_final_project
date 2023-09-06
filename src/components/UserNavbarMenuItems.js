08cdd2aebbd1d87f09dbab15b5f1c83e360dd60f
feldib <beni.feldmayer@gmail.com>
2023-09-06 15:42:03 +0200

    split navbar into components

---
+++ src/components/UserNavbarMenuItems.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function UserNavbarMenuItems(props) {
+    return (
+        <>
+            <Link className='nav-link' style={{ color: 'inherit', textDecoration: 'inherit'}} to="/shopping_cart">
+                Shopping cart
+            </Link>
+
+            <Link className='nav-link' style={{ color: 'inherit', textDecoration: 'inherit'}} to="/user">
+                {props.first_name}'s Page
+            </Link>
+        </>
+    )
+}
+
+export default UserNavbarMenuItems
