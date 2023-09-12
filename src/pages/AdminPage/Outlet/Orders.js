05134ea83badd33b3167ccc313094e893ba5d027
feldib <beni.feldmayer@gmail.com>
2023-09-12 10:50:41 +0200

    now orders appear on admin's page

--- src/pages/AdminPage/Outlet/Orders.js
+++ src/pages/AdminPage/Outlet/Orders.js
@@ -1,246 +1,30 @@
 import React from 'react'
 import { Col, Row, Table} from 'react-bootstrap'
+import useAxios from '../../../hooks/useAxios'
+import useLoading from '../../../hooks/useLoading'
+import { admin_url } from '../../../utils/api_constants'
+import OrderSummaryComponent from '../../../components/OrderSummaryComponent'
 
 function Orders() {
+    function representOrderDataCollection(orderDataCollection){
+        const len = orderDataCollection.length
+        return orderDataCollection.map((orderData, index)=>{
+            return (
+                <OrderSummaryComponent 
+                    title={`Order ${len-index}`}
+                    items={orderData.items}
+                    totalCost={orderData.totalCost}
+                    user={orderData.user}
+                />
+            )
+        })
+    }
+
+    const orders = useAxios(`/${admin_url}/get_orders`)
+    const ordersRepresented = useLoading(orders, representOrderDataCollection)
     return (
-        <Col>
-            <Row>
-                <Row>
-                    <Row className='mb-2 mt-5'>
-                        <h3 className='text-left'>Order 1</h3>
-                    </Row>
-
-                    <Row>
-                        <Table className='table-hover'>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Title</th>
-                                    <th>Artist</th>
-                                    <th>Quantity</th>
-                                    <th>Price (€)</th>
-                                    <th>Total Cost (€)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <img 
-                                                src="https://d7hftxdivxxvm.cloudfront.net/?height=600&quality=80&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Fpj1Kk4Od1CBV8tWBLk3zeA%2Fnormalized.jpg&width=800" 
-                                                height="100"
-                                                style={{objectFit: "contain"}}
-                                            />
-                                    </td>
-                                    <td>
-                                        Birth of Venus
-                                    </td>
-                                    <td>
-                                        Boticelli
-                                    </td>
-                                    <td>
-                                        1
-                                    </td>
-                                    <td>
-                                        45
-                                    </td>
-                                    <td>
-                                        45
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <img 
-                                                src="https://d7hftxdivxxvm.cloudfront.net/?height=600&quality=80&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Fpj1Kk4Od1CBV8tWBLk3zeA%2Fnormalized.jpg&width=800" 
-                                                height="100"
-                                                style={{objectFit: "contain"}}
-                                            />
-                                    </td>
-                                    <td>
-                                        Birth of Venus
-                                    </td>
-                                    <td>
-                                        Boticelli
-                                    </td>
-                                    <td>
-                                        2
-                                    </td>
-                                    <td>
-                                        45
-                                    </td>
-                                    <td>
-                                        90
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </Row>
-                </Row>
-
-                <Row className='mt-4'>
-                    <h2>
-                        Order Summary: € 179
-                    </h2>
-                </Row>
-            </Row>
-
-            <Row>
-                <Row>
-                    <Row className='mb-2 mt-5'>
-                        <h3 className='text-left'>Order 2</h3>
-                    </Row>
-
-                    <Row>
-                        <Table className='table-hover'>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Title</th>
-                                    <th>Artist</th>
-                                    <th>Quantity</th>
-                                    <th>Price (€)</th>
-                                    <th>Total Cost (€)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <img 
-                                                src="https://d7hftxdivxxvm.cloudfront.net/?height=600&quality=80&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Fpj1Kk4Od1CBV8tWBLk3zeA%2Fnormalized.jpg&width=800" 
-                                                height="100"
-                                                style={{objectFit: "contain"}}
-                                            />
-                                    </td>
-                                    <td>
-                                        Birth of Venus
-                                    </td>
-                                    <td>
-                                        Boticelli
-                                    </td>
-                                    <td>
-                                        1
-                                    </td>
-                                    <td>
-                                        45
-                                    </td>
-                                    <td>
-                                        45
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <img 
-                                                src="https://d7hftxdivxxvm.cloudfront.net/?height=600&quality=80&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Fpj1Kk4Od1CBV8tWBLk3zeA%2Fnormalized.jpg&width=800" 
-                                                height="100"
-                                                style={{objectFit: "contain"}}
-                                            />
-                                    </td>
-                                    <td>
-                                        Birth of Venus
-                                    </td>
-                                    <td>
-                                        Boticelli
-                                    </td>
-                                    <td>
-                                        2
-                                    </td>
-                                    <td>
-                                        45
-                                    </td>
-                                    <td>
-                                        90
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </Row>
-                </Row>
-
-                <Row className='mt-4'>
-                    <h2>
-                        Order Summary: € 179
-                    </h2>
-                </Row>
-            </Row>
-
-            <Row>
-                <Row>
-                    <Row className='mb-2 mt-5'>
-                        <h3 className='text-left'>Order 3</h3>
-                    </Row>
-
-                    <Row>
-                        <Table className='table-hover'>
-                            <thead>
-                                <tr>
-                                    <th></th>
-                                    <th>Title</th>
-                                    <th>Artist</th>
-                                    <th>Quantity</th>
-                                    <th>Price (€)</th>
-                                    <th>Total Cost (€)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <tr>
-                                    <td>
-                                        <img 
-                                                src="https://d7hftxdivxxvm.cloudfront.net/?height=600&quality=80&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Fpj1Kk4Od1CBV8tWBLk3zeA%2Fnormalized.jpg&width=800" 
-                                                height="100"
-                                                style={{objectFit: "contain"}}
-                                            />
-                                    </td>
-                                    <td>
-                                        Birth of Venus
-                                    </td>
-                                    <td>
-                                        Boticelli
-                                    </td>
-                                    <td>
-                                        1
-                                    </td>
-                                    <td>
-                                        45
-                                    </td>
-                                    <td>
-                                        45
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <img 
-                                                src="https://d7hftxdivxxvm.cloudfront.net/?height=600&quality=80&resize_to=fit&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2Fpj1Kk4Od1CBV8tWBLk3zeA%2Fnormalized.jpg&width=800" 
-                                                height="100"
-                                                style={{objectFit: "contain"}}
-                                            />
-                                    </td>
-                                    <td>
-                                        Birth of Venus
-                                    </td>
-                                    <td>
-                                        Boticelli
-                                    </td>
-                                    <td>
-                                        2
-                                    </td>
-                                    <td>
-                                        45
-                                    </td>
-                                    <td>
-                                        90
-                                    </td>
-                                </tr>
-                            </tbody>
-                        </Table>
-                    </Row>
-                </Row>
-
-                <Row className='mt-4'>
-                    <h2>
-                        Order Summary: € 179
-                    </h2>
-                </Row>
-            </Row>
-
+        <Col className='mb-5 pb-5'>
+            {ordersRepresented}
         </Col>
     )
 }
