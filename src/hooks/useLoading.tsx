367e13e1e2d635ee91b78c3dbdab06fb10c8a962
feldib <beni.feldmayer@gmail.com>
2025-09-07 13:38:58 +0200

    fixed useLoading

--- src/hooks/useLoading.tsx
+++ src/hooks/useLoading.tsx
@@ -1,10 +1,7 @@
 "use client";
 import React from "react";
 
-const useLoading = (
-  data: unknown,
-  makeJSX: (data: unknown) => React.JSX.Element
-) => {
+const useLoading = <T,>(data: T, makeJSX: (data: T) => React.JSX.Element) => {
   const spinner = (
     <div className="d-flex justify-content-center">
       <div className="spinner-border" role="status" />
@@ -18,6 +15,7 @@ const useLoading = (
     } else {
       setResult(spinner);
     }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [data]);
 
   return result;
