8cd689d0ee54fbd2c3fb6ecf06b3e42af84c4b7e
feldib <beni.feldmayer@gmail.com>
2025-09-30 13:08:19 +0200

    fixed language selector width

--- src/components/navbars/LanguageSelector.tsx
+++ src/components/navbars/LanguageSelector.tsx
@@ -2,51 +2,46 @@
 
 import { HU, IL, US } from "country-flag-icons/react/3x2";
 
-import { Col, Row } from "react-bootstrap";
-
 import { useI18n } from "@/components/providers/I18nProvider";
 
 const LanguageSelector = () => {
   const { locale, setLocale } = useI18n();
 
   return (
-    <Row>
-      <Col>
-        <US
-          className="flag-icon"
-          height="18"
-          onClick={() => setLocale("en")}
-          style={{
-            opacity: locale === "en" ? 1 : 0.6,
-            cursor: "pointer",
-          }}
-        />
-      </Col>
+    <div
+      className="d-flex justify-content-start align-items-center w-100"
+      style={{ gap: "12px" }}
+    >
+      <US
+        className="flag-icon"
+        height="18"
+        onClick={() => setLocale("en")}
+        style={{
+          opacity: locale === "en" ? 1 : 0.6,
+          cursor: "pointer",
+        }}
+      />
 
-      <Col>
-        <IL
-          className="flag-icon"
-          height="18"
-          onClick={() => setLocale("he")}
-          style={{
-            opacity: locale === "he" ? 1 : 0.6,
-            cursor: "pointer",
-          }}
-        />
-      </Col>
+      <IL
+        className="flag-icon"
+        height="18"
+        onClick={() => setLocale("he")}
+        style={{
+          opacity: locale === "he" ? 1 : 0.6,
+          cursor: "pointer",
+        }}
+      />
 
-      <Col>
-        <HU
-          className="flag-icon"
-          height="18"
-          onClick={() => setLocale("hu")}
-          style={{
-            opacity: locale === "hu" ? 1 : 0.6,
-            cursor: "pointer",
-          }}
-        />
-      </Col>
-    </Row>
+      <HU
+        className="flag-icon"
+        height="18"
+        onClick={() => setLocale("hu")}
+        style={{
+          opacity: locale === "hu" ? 1 : 0.6,
+          cursor: "pointer",
+        }}
+      />
+    </div>
   );
 };
 
