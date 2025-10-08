6d6168dcafd4b9832dfd50eefc8a50eac3507690
feldib <beni.feldmayer@gmail.com>
2025-10-08 20:28:19 +0200

    improved accessiblity

--- src/components/accessibility/SkipToContentLink.tsx
+++ src/components/accessibility/SkipToContentLink.tsx
@@ -1,10 +1,13 @@
 "use client";
+import React from "react";
+
 import { useI18n } from "../providers/I18nProvider";
 
 const SkipToContentLink = () => {
   const { t } = useI18n();
+
   return (
-    <a className="skip-to-content" href="#main">
+    <a className="skip-to-content" href="#main" tabIndex={0}>
       {t("common.skip_to_content")}
     </a>
   );
